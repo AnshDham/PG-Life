5f6bb264d06f5f7c2a451836ca3cdc8cb928b5a0
Yash Devani <yashdewani159@gmail.com>
2025-06-16 12:22:31 +0530

    error resolving

--- backend/scr/Middleware/auth.middleware.js
+++ backend/scr/Middleware/auth.middleware.js
@@ -1,4 +1,6 @@
-
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 export const authMiddleware = (req, res, next) => {
     try {
         const token = req.cookies.token;
